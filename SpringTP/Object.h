2a22c4681e4f0fc844a3f9538e1e29f287bc2221
tarou <tatou@outlook.jp>
2023-01-20 10:49:32 +0900

    一旦保存

---
+++ SpringTP/Object.h
@@ -0,0 +1,12 @@
+#pragma once
+#include"game.h"
+class Object
+{
+public:
+    Object();
+    virtual ~Object();
+
+protected:
+    VECTOR pos;
+};
+
---
+++ SpringTP/game.h
@@ -0,0 +1,9 @@
+#pragma once
+#include"DxLib.h"
+
+const int WONDOW_WIDTH = 1920;
+const int WINDOW_HEIGHT = 1080;
+
+const float ZERO_F = 0.0f;
+const int ZERO_I = 0;
+const VECTOR ZERO_POS = VGet(0.0f, 0.0f, 0.0f);
